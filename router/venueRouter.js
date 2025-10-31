1bf6e44ba5f9c1deb79221df5f4966b603803c08
godwinkalu <kalugodwin158@gmail.com>
2025-10-31 01:59:22 +0100

    updated

--- router/venueRouter.js
+++ router/venueRouter.js
@@ -135,11 +135,11 @@ router.post('/list-venue', authentication, upload.array('image', 5), createVenue
 
 /**
  * @swagger
- * /venues/all:
+ * /allvenues:
  *   get:
  *     summary: Get all venues
  *     description: Retrieve all venues listed in the platform.
- *     tags: [Venues]
+ *     tags: [Venue]
  *     responses:
  *       200:
  *         description: List of all venues retrieved successfully
@@ -148,11 +148,11 @@ router.get('/allvenues', getAllVenues)
 
 /**
  * @swagger
- * /venues/getOneVenue/{id}:
+ * /getOneVenue/{id}:
  *   get:
  *     summary: Get one venue by ID
  *     description: Retrieve details of a specific venue.
- *     tags: [Venues]
+ *     tags: [Venue]
  *     parameters:
  *       - in: path
  *         name: id
@@ -260,7 +260,7 @@ router.put('/upload-docs', authentication, upload.fields([
  *   put:
  *     summary: Update a venue
  *     description: Allows a venue owner to update their venue details.
- *     tags: [Venues]
+ *     tags: [Venue]
  *     security:
  *       - bearerAuth: []
  *     parameters:
@@ -290,11 +290,11 @@ router.put('/upload-docs', authentication, upload.fields([
 router.put('/updatedvenue/:id', authentication, upload.array('image', 5), updateVenue)
 /**
  * @swagger
- * /venues/deletevenue/{id}:
+ * /deletevenue/{id}:
  *   delete:
  *     summary: Delete a venue
  *     description: Allows a venue owner to delete a venue.
- *     tags: [Venues]
+ *     tags: [Venue]
  *     security:
  *       - bearerAuth: []
  *     parameters:
